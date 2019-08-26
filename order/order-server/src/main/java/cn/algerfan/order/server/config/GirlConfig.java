8a3abe171f7730a2f810d4707a71aef98aaf440d
AlgerFan <fsj.wlgzs@gmail.com>
2019-08-26 21:51:16 +0800

    Spring Cloud Bus的使用

---
+++ order/order-server/src/main/java/cn/algerfan/order/server/config/GirlConfig.java
@@ -0,0 +1,21 @@
+package cn.algerfan.order.server.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author algerfan
+ * @time 2019.08.26 10: 47
+ */
+@Data
+@Component
+@ConfigurationProperties("girl")
+@RefreshScope
+public class GirlConfig {
+
+    private String name;
+
+    private Integer age;
+}
