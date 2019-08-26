30a4f2c559ed342e541bbede1cd333fb2be2fa0d
AlgerFan <fsj.wlgzs@gmail.com>
2019-08-26 21:35:52 +0800

    Config Client

---
+++ order/order-server/src/main/java/cn/algerfan/order/server/controller/EnvController.java
@@ -0,0 +1,25 @@
+package cn.algerfan.order.server.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author algerfan
+ * @time 2019.08.23 11: 47
+ */
+@RestController
+@RequestMapping("/env")
+@RefreshScope
+public class EnvController {
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/print")
+    public String print() {
+        return env;
+    }
+}
