8546c665d464be2735c5b8cf5a78d3ed5f33f78b
AlgerFan <fsj.wlgzs@gmail.com>
2019-08-26 21:44:34 +0800

    Spring Cloud Bus的使用

---
+++ order/order-server/src/main/java/cn/algerfan/order/server/controller/GirlController.java
@@ -0,0 +1,23 @@
+package cn.algerfan.order.server.controller;
+
+import cn.algerfan.order.config.GirlConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author algerfan
+ * @time 2019/8/26 10:46
+ */
+@RestController
+public class GirlController {
+
+    @Autowired
+    private GirlConfig girlConfig;
+
+    @RequestMapping("/girl/print")
+    public String girl() {
+        return "name: "+girlConfig.getName()+ "  age: "+girlConfig.getAge();
+    }
+
+}
