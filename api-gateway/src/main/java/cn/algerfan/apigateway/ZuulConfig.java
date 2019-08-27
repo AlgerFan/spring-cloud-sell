21e426899250d1fd83591ef19bc7fe22e5055001
AlgerFan <fsj.wlgzs@gmail.com>
2019-08-27 20:59:21 +0800

    服务网关Zuul的简单使用

---
+++ api-gateway/src/main/java/cn/algerfan/apigateway/ZuulConfig.java
@@ -0,0 +1,21 @@
+package cn.algerfan.apigateway;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author algerfan
+ * @time 2019/8/27 20:46
+ */
+@Component
+public class ZuulConfig {
+
+    @ConfigurationProperties("zuul")
+    @RefreshScope
+    public ZuulProperties zuulProperties(){
+        return new ZuulProperties();
+    }
+
+}
